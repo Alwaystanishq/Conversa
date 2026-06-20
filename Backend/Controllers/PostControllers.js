76789bd482784058f157862d30aa8777d0b4d971
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-06-20 11:37:42 +0530

    Delete Post Controller

--- Backend/Controllers/PostControllers.js
+++ Backend/Controllers/PostControllers.js
@@ -91,21 +91,47 @@ export const unlikePostController = async (req, res) => {
       },
       { new: true },
     );
-
     if (!post) {
       return res.status(404).json({
         success: false,
         message: "Post not found",
       });
     }
-
     return res.status(200).json({
       success: true,
       message: "Post unliked",
     });
   } catch (error) {
     console.error(`Server error in unlikePostController: ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
+export const deletePostController = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+    if (post.author.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    await post.deleteOne();
+    return res.status(200).json({
+      success: true,
+      message: "Post deleted successfully",
+    });
+  } catch (error) {
+    console.error(`Server error in deletePostController: ${error.message}`);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
