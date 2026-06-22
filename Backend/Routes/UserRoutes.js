d22f30c3a2f9994ee8c40098755fa0d5da4d47f8
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-06-22 21:21:22 +0530

    Search User route

--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   followUserController,
   getUserProfileController,
+  searchUsersController,
   unfollowUserController,
 } from "../Controllers/UserControllers";
 
 const router = express.Router();
 
+router.get("/search", searchUsersController);
 router.get("/:username", getUserProfileController);
 router.put("/:userId/follow", followUserController);
 router.put("/:userId/unfollow", unfollowUserController);
