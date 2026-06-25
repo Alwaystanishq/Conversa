0bca33fde85e2ad41980bd3e5f1675b356488e50
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-06-25 21:23:13 +0530

    Rate limiter

--- Backend/server.js
+++ Backend/server.js
@@ -2,24 +2,42 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import { connectDB } from "./Config/DB";
 import AuthRouter from "./Routes/AuthRoutes";
 import { authToken } from "./Middlewares/AuthMiddleware";
 import PostRouter from "./Routes/PostRoutes";
 import UserRouter from "./Routes/UserRoutes";
 
-const app = express();
 dotenv.config();
+
+const app = express();
+
 connectDB();
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   }),
 );
-app.use(express.json());
 
-app.use("/api/auth", AuthRouter);
+app.use(express.json());
+app.use(cookieParser());
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    success: false,
+    message: "Too many requests. Please try again after 15 minutes.",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("/api/auth", authLimiter, AuthRouter);
+
 app.use("/api/posts", authToken, PostRouter);
 app.use("/api/users", authToken, UserRouter);
 
