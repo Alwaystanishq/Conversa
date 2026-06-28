280afd4d462bf6e0b702daef6cd961ca19ef818e
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-06-28 13:57:28 +0530

    Routing

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+export default function App() {
   return (
-    <div>App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Login />} />
 
-export default App
+      <Route path="/register" element={<Register />} />
+
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/create"
+        element={
+          <ProtectedRoute>
+            <Create />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
